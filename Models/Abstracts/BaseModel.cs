dc971519245061d375e814da24e6407aef4e20cc
Arek Stepnik <arkadiusz.stepnik88@gmail.com>
2023-04-08 17:18:34 +0200

    Repository creation - EstateZoningApp

---
+++ Models/Abstracts/BaseModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EstateZoningApp.Core.Models.Abstracts;
+public abstract class BaseModel: INotifyPropertyChanged, INotifyPropertyChanging
+{
+    protected void OnPropertyChanged([CallerMemberName] String propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected void OnPropertyChanging([CallerMemberName] String propertyName = "")
+    {
+        PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+    }
+
+    public event PropertyChangingEventHandler PropertyChanging;
+    public event PropertyChangedEventHandler PropertyChanged;
+}
---
+++ Models/Abstracts/SimplePoint.cs
@@ -0,0 +1,59 @@
+﻿
+namespace EstateZoningApp.Core.Models.Abstracts;
+public abstract class SimplePoint : BaseModel
+{
+    protected SimplePoint()
+    {
+    }
+
+    public int Id
+    {
+        get;
+        set;
+    }
+
+    double _X;
+    public double X
+    {
+        get => _X;
+        set
+        {
+            if (_X != value)
+            {
+                OnPropertyChanging(nameof(X));
+                _X = value;
+                OnPropertyChanged(nameof(X));
+            }
+        }
+    }
+
+    double _Y;
+    public double Y
+    {
+        get => _Y;
+        set
+        {
+            if (_Y != value)
+            {
+                OnPropertyChanging(nameof(Y));
+                _Y = value;
+                OnPropertyChanged(nameof(Y));
+            }
+        }
+    }
+
+    int _IconId;
+    public int IconId
+    {
+        get => _IconId;
+        set
+        {
+            if (_IconId != value)
+            {
+                OnPropertyChanging(nameof(IconId));
+                _IconId = value;
+                OnPropertyChanged(nameof(IconId));
+            }
+        }
+    }
+}
